c7672cfc059c788b1d8e5dec046c2315efbe35b1
Zorych <kaze250t@gmail.com>
2019-12-07 21:59:57 +0300

    выбор конкретной группы для удаления/изменения

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -13,12 +13,12 @@ public class GroupModificationTests extends TestBase {
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
     }
-    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().selectGroup(before - 1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("test1", "test5", "test6"));
     app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
     int after = app.getGroupHelper().getGroupCount();
     Assert.assertEquals(after, before);
-    app.getGroupHelper().returnToGroupPage();
   }
 }
