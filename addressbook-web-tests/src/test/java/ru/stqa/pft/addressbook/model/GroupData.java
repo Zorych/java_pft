6560d05dc414b12f69d23510c6e9b48f57a1ee77
Zorych <kaze250t@gmail.com>
2019-12-08 20:01:24 +0300

    лямбды и сортировка

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -3,14 +3,31 @@ package ru.stqa.pft.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
+  private int id;
   private final String name;
   private final String header;
   private final String footer;
 
-  public GroupData(String name, String header, String footer) {
+  public GroupData(int id, String name, String header, String footer) {
+    this.id = id;
     this.name = name;
     this.header = header;
     this.footer = footer;
+  }
+
+  public GroupData(String name, String header, String footer) {
+    this.id = Integer.MAX_VALUE;
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
   }
 
   public String getName() {
@@ -26,13 +43,6 @@ public class GroupData {
   }
 
   @Override
-  public String toString() {
-    return "GroupData{" +
-                   "name='" + name + '\'' +
-                   '}';
-  }
-
-  @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
@@ -44,4 +54,13 @@ public class GroupData {
   public int hashCode() {
     return Objects.hash(name);
   }
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+                   "id=" + id +
+                   ", name='" + name + '\'' +
+                   '}';
+  }
+
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,16 +4,36 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() throws Exception {
+    // открываем страницу со списком групп
     app.getNavigationHelper().goToGroupPage();
+
+    // сохраняем список имеющихся групп (до создания новой группы)
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+
+    // создаём объект типа GroupData и указываем данные для новой группы
+    GroupData group = new GroupData("test1", "test2", "test3");
+
+    // вызываем метод создания группы
+    app.getGroupHelper().createGroup(group);
+
+    // сохраняем список групп после создания новой группы
     List<GroupData> after = app.getGroupHelper().getGroupList();
+
+    // проверяем, что после создания кол-во групп стало +1
     Assert.assertEquals(after.size(), before.size() + 1);
+
+    before.add(group);
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
   }
 }
