bae1978c4759f1c8cbcafa46c0b7ce5c7184e0ea
Zorych <kaze250t@gmail.com>
2019-12-21 20:55:43 +0300

    Задание №13: Параметризовать тесты для создания групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,35 +1,40 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test
-  public void testContactCreation() throws Exception {
-    ContactData contact = new ContactData()
-                                  .withFirstName("Игорь")
-                                  .withMiddleName("Сергеевич")
-                                  .withLastName("Григорьев")
-                                  .withAddress("ул. Каретный ряд 10/2, 2 этаж")
-                                  .withNickName("Zorych")
-                                  .withTitle("test_title")
-                                  .withCompany("test_company")
-                                  .withHomePhone("+7 (111)11-11")
-                                  .withMobilePhone("2-2 2-2 22")
-                                  .withWorkPhone("333333")
-                                  .withEmail("kaze250t@gmail.com")
-                                  .withEmail3("zorych250t@yandex.ru")
-                                  .withHomepage("https://www.linkedin.com/in/igor-grigorev-3a657a18b/")
-                                  .withBirthday("11", "May", "1991")
-                                  .withAnniversary("15", "April", "1992")
-                                  .withSecAddress("test_secondary-address")
-                                  .withSecPhone("555555")
-                                  .withSecNotes("test_secondary-notes");
+  @DataProvider
+  public Iterator<Object[]> validContactsFromJson() throws IOException {
+//    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+    BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("src/test/resources/contacts.json"), "cp1251"));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null) {
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType()); //List<ContactData>.class
+    return contacts.stream().map((c) -> new Object[] {c}).collect(Collectors.toList()).iterator();
+  }
+
+  @Test(dataProvider = "validContactsFromJson")
+  public void testContactCreation(ContactData contact) throws Exception {
     app.goTo().homePage();
     Contacts before = app.contact().all();
     app.contact().create(true, contact);
